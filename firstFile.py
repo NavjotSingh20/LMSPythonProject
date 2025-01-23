0bf6eb35a7fae43aa7bbdd90bd27079d041e88cf
NavjotSingh20 <navjothans2005@gmail.com>
2025-01-23 21:38:29 +0530

    test commit

--- firstFile.py
+++ firstFile.py
@@ -1,1 +1,15 @@
-print("Testing")
+class Books:
+    books_title = {}
+    books_author = {}
+    
+    def __init__(self, title, author, copies):
+        Books.title = title
+        Books.author =  author
+        Books.copies = copies
+
+        
+
+
+
+book1 = Books('Test', 'Test 2', 300)
+ 
