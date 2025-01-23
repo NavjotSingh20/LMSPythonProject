75ec8912c96a136b05abdd5ef81c3158f7bf4bd8
NavjotSingh20 <navjothans2005@gmail.com>
2025-01-23 22:10:51 +0530

    Base class declared and basic get commands done

--- firstFile.py
+++ firstFile.py
@@ -3,13 +3,30 @@ class Books:
     books_author = {}
     
     def __init__(self, title, author, copies):
-        Books.title = title
-        Books.author =  author
-        Books.copies = copies
+        self.title = title
+        self.author =  author
+        self.copies = copies
 
-        
+        Books.books_title[title] = self
+        Books.books_author[author] = self
+
+    @classmethod
+    def get_author_by_title(cls, title):
+        book = cls.books_title.get(title)
+        if book:
+            return book.author
+        else:
+            return "Book not found"
+
+    @classmethod
+    def get_title_by_author(cls, author):
+        book = cls.books_author.get(author)
+        if book:
+            return book.title
+        else:
+            return "Book not found"
 
 
 
 book1 = Books('Test', 'Test 2', 300)
- 
+print(Books.get_author_by_title('Test'))
