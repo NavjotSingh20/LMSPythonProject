7f664ca216555704992a3a1a897e9a91c613afe8
manviii345 <manvigajrani22@gmail.com>
2025-01-27 14:57:07 +0530

    Update adminlogin.py

--- adminlogin.py
+++ adminlogin.py
@@ -1,53 +1,52 @@
 #dictionary to store admin credentials 
+#dictionary to store admin credentials
 admins={
-    "admin1":"password1"
+    "admin1":"password1",
     "admin2":"password2"
 }
- def admin_login():
+def admin_login():
      print ("admin login")
-     username=input("enter admin username:")
-     password=input("enter admin password:")
-    if username in admins and admins[username]==paassword:
+username=input("enter admin username:")
+password=input("enter admin password:")
+if username in admins and admins[username]==paassword:
         print("login successful")
-    else:
+else:
         print("invalid credentials")
 
- def add_new_book():
+def add_new_book():
      print("add books")
      book_title=input("enter book title")
      book_author=input("enter book author")
      book_quantity=int(input("enter quantity"))
      if book_title in books_title:
-         print ("book laready exists ,use restock to add more books")
+         print ("book already exists ,use restock to add more books")
      else:
          print("added successfully")
 
- def restock_book():
+def restock_book():
       print("restock books")
       book_title=input("enter title")
-       if book_title in books_title:
-           additional_copies=int(input("enter the number of copies")
-            book_title.copies+=additional_copies
-       else:
+      if book_title in books_title:
+           additional_copies=int(input("enter the number of copies"))
+           book_title.copies+=additional_copies
+      else:
            print("no such book is available")
 
- def admin_menu():
+def admin_menu():
      while True:
          print("1 : add new book")
          print("2 : restock book")
          print("3 : logout")
 
-      choice=int(input("enter your choice"))
-       if choice==1:
+     choice=int(input("enter your choice"))
+     if choice==1:
            add_new_book()
-       elif choice==2:
+     elif choice==2:
            restock_book()
-       elif choice==3:
+     elif choice==3:
            print("logging out")
-       else:
+     else:
            print("invalid choice")
 
-    def main():
+def main():
         print("welcome")
-        
-                
